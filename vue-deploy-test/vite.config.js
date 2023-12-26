4f06563fff085dd10f48402ddc6df7db9945e508
azechi_yoshikazu_atg <azechi.yoshikazu@arstechguild.inc>
2023-12-26 16:44:34 +0900

    fix: base url

--- vue-deploy-test/vite.config.js
+++ vue-deploy-test/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://azechi-yoshikazu-gs.github.io/VueDeployTest/",
+  base: "/VueDeployTest/",
   plugins: [
     vue(),
   ],
